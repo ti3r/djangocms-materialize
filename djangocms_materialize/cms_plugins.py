d9e4554f9cdb9309307fb1d49f13ad11f20eeb14
Alexandro Blanco <ti3r.bubblenet@gmail.com>
2016-06-22 14:24:07 -0500

    Adding more work towards cards components

--- djangocms_materialize/cms_plugins.py
+++ djangocms_materialize/cms_plugins.py
@@ -1,7 +1,7 @@
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
 from django.utils.translation import ugettext_lazy as _
-from .models import MatButtonModel, MatChipTagModel
+from .models import MatButtonModel, MatChipTagModel, MatCardContentModel, MatCardModel
 
 
 class MatButton(CMSPluginBase):
@@ -25,5 +25,34 @@ class MatChipTag(CMSPluginBase):
     cache = True
     model = MatChipTagModel
 
+class MatCardContent(CMSPluginBase):
+    """
+    Plugin to add one row of content to a card
+    """
+    module = _("Materialize")
+    name = _("Card Content")
+    render_template = 'djangocms_materialize/components/card-content.html'
+    cache = True
+    require_parent = True
+    parent_classes = ['MatCard']
+    model = MatCardContentModel
+    allow_children = True
+    #child_classes = ['text']
+
+class MatCard(CMSPluginBase):
+    """
+    Plugin to display a card component
+    """
+    module = _("Materialize")
+    name = _("Card")
+    render_template = 'djangocms_materialize/components/card.html'
+    cache = True
+    allow_children = True
+    child_classes = ['MatCardContent']
+    model = MatCardModel
+
+
 plugin_pool.register_plugin(MatButton)
 plugin_pool.register_plugin(MatChipTag)
+plugin_pool.register_plugin(MatCardContent)
+plugin_pool.register_plugin(MatCard)
--- djangocms_materialize/models.py
+++ djangocms_materialize/models.py
@@ -23,6 +23,19 @@ MAT_BUTTON_ICON_POSITION_CHOICES = (
     ("right", _("Right")),
 )
 
+MAT_CARD_SIZE_CHOICES = (
+    ("small",_("Small")),
+    ("medium",_("Medium")),
+    ("large",_("Large")),
+)
+
+MAT_CARD_CONTENT_MODES = (
+    ("content",_("Content")),
+    ("action",_("Action")),
+    ("image", _("Image")),
+    ("reveal",_("Reveal (Experimental)")),
+)
+
 ##########
 # Mixins #
 ##########
@@ -53,8 +66,8 @@ class ButtonMixin(models.Model):
             link = self.link_url
         elif self.link_page_id:
             link = self.link_page.get_absolute_url()
-        elif self.link_file:
-            link = self.link_file.url
+        #elif self.link_file:
+        #    link = self.link_file.url
         else:
             link = ""
         if self.link_anchor:
@@ -78,14 +91,17 @@ class MatButtonModel(CMSPlugin, ButtonMixin):
                                      ,choices=MAT_BUTTON_ICON_POSITION_CHOICES)
 
     def get_btn_class(self):
+        clazz = u''
         if self.mode == 'F1':
-            return u'btn-floating'
+            clazz = u'btn-floating'
         elif self.mode == 'F2':
-            return u'btn-flat'
+            clazz = u'btn-flat'
         elif self.large:
-            return u'btn-large'
+            clazz = u'btn-large'
         else:
-            return u'btn'
+            clazz = u'btn'
+        clazz += ' {ec}'.format(ec=self.extra_classes)
+        return clazz
 
     def is_disabled(self):
         return u'disabled' if self.disabled else u''
@@ -101,3 +117,56 @@ class MatChipTagModel(CMSPlugin):
                                  verbose_name=_('Alternate text for image'))
     is_tag = models.BooleanField(default=False, verbose_name=_('Is a Tag'))
 
+
+class MatCardModel(CMSPlugin):
+    """
+    Model class to store the configuration of the Card plugin.
+    """
+    size = models.CharField(max_length=100, default='small', blank=True, null=True, verbose_name=_('Size'),
+                            choices=MAT_CARD_SIZE_CHOICES)
+    is_panel = models.BooleanField(default=False, verbose_name=_("Is Panel"),
+                                    help_text=_("Mark this card panel (more simple card component). More info see: \
+                                    http://materializecss.com/cards.html"))
+    extra_classes = models.TextField(null=True, blank=True,
+                                     help_text=_('Add extra class names that you wish to add to the component')
+                                     , verbose_name=_('Extra Classes'))
+
+    def get_card_classes(self):
+        classes = u'card'
+        if self.is_panel:
+            classes += u'-panel'
+        classes += ' {size} '.format(size=self.size)
+        classes += ' {ec} '.format(ec=self.extra_classes)
+        return classes
+
+
+class MatCardContentModel(CMSPlugin):
+    """
+    Model class to store the configuration of the different card contents
+    """
+    title = models.CharField(max_length=100, default='', blank=True, null=True, verbose_name=_('Title'))
+    mode = models.CharField(default='', verbose_name=_("Mode"), choices=MAT_CARD_CONTENT_MODES, max_length=20,
+                                    help_text=_("Mark the style of the content. More info see: \
+                                    http://materializecss.com/cards.html"))
+    extra_classes = models.TextField(null=True, blank=True,
+                                     help_text=_('Add extra class names that you wish to add to the component')
+                                     , verbose_name=_('Extra Classes'))
+
+    def get_content_class(self):
+        """
+        returns the class that should be applied to the card content row based on the different flags for the plugin.
+        :return: unicode materialize class name
+        """
+        clazz = u'card-{mode}'
+        if self.mode:
+            clazz = clazz.format(mode=self.mode)
+        else:
+            clazz = clazz.format(mode='content')
+        return clazz
+
+    def get_extra_classes(self):
+        """
+        returns the list of extra class names that should be applied to the card content template
+        :return:
+        """
+        return self.extra_classes
